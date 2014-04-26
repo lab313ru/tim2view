a2f2ec1944f41942888bcd1ac2f5d4e178b85616
meffi@lab313.ru <meffi@lab313.ru@706930b9-3d88-df67-00fe-4c1b76c3f905>
2014-04-26 12:59:46 +0000

    Debug fixes. Not for update.

--- trunk/uexportimport.pas
+++ trunk/uexportimport.pas
@@ -2,12 +2,17 @@ unit uexportimport;
 
 interface
 
-uses FPWritePNG, zstream, udrawtim;
+uses
+  FPWritePNG, zstream, udrawtim;
 
 procedure SaveImage(Surf: PDrawSurf; Indexed: Boolean; const FileName: string; PngWriter: TFPWriterPNG);
+function LoadImage(const FileName: string; var Surf: PDrawSurf): Boolean;
 function CreatePngWriter(ForExport: Boolean): TFPWriterPNG;
 
 implementation
+
+uses
+  FPReadPNG, Classes, BGRABitmap, FileUtil, sysutils;
 
 function CreatePngWriter(ForExport: Boolean): TFPWriterPNG;
 begin
@@ -21,6 +26,32 @@ procedure SaveImage(Surf: PDrawSurf; Indexed: Boolean; const FileName: string; P
 begin
   PngWriter.Indexed := Indexed;
   Surf^.SaveToFileUTF8(FileName, PngWriter);
+end;
+
+function LoadImage(const FileName: string; var Surf: PDrawSurf): Boolean;
+var
+  Reader: TFPReaderPNG;
+  Image: TFileStream;
+begin
+  Result := False;
+
+  try
+  Reader := TFPReaderPNG.Create;
+  Image := TFileStream.Create(UTF8ToSys(FileName), fmOpenRead or fmShareDenyWrite);
+  Result := Reader.CheckContents(Image);
+
+  if Result then
+  begin
+    New(Surf);
+    Surf^ := TBGRABitmap.Create;
+    Image.Position := 0;
+    Surf^.LoadFromStream(Image);
+  end;
+
+  finally
+    Image.Free;
+    Reader.Free;
+  end;
 end;
 
 end.
